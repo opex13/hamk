cb8a04a72d92210105cc8b600e42c4e712c73e4a
opex13 <97477580+opex13@users.noreply.github.com>
2022-08-27 21:38:29 +0300

    tehtävä 1 ok

--- ohjelmointi_perusteet/tehtava1.py
+++ ohjelmointi_perusteet/tehtava1.py
@@ -1,1 +1,7 @@
-print("Moi! Annapas kaksi ")
+print("Moi! Annapas kaksi lukua")
+NumeroA = int(input("Ensimmäinen luku:"))
+NumeroB = int(input("Toinen luku:"))
+print("Näiden summa on:", NumeroA, "+", NumeroB, "=", NumeroA+NumeroB)
+print("Erotus on:", NumeroA, "-", NumeroB, "=", NumeroA-NumeroB)
+print("Tulo on:", NumeroA, "*", NumeroB, "=", NumeroA*NumeroB)
+print("Osamäärä on:", NumeroA, "/", NumeroB, "=", NumeroA/NumeroB)
