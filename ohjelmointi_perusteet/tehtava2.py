68d4742e1a413796057be3f12026630ea39e190d
opex13 <97477580+opex13@users.noreply.github.com>
2022-08-27 22:36:28 +0300

    näppäilyvirhe

--- ohjelmointi_perusteet/tehtava2.py
+++ ohjelmointi_perusteet/tehtava2.py
@@ -1,6 +1,6 @@
 #ohjelmointi perusteet tehävä 2
 TilinSaldo = int(500)
-Tulot = int(input("Paljonko tuki tukea?"))
+Tulot = int(input("Paljonko tuli tukea?"))
 TilinSaldo = TilinSaldo+Tulot
 Menot = int(input("Paljonko tuhlattu viikonloppuna?"))
 TilinSaldo = TilinSaldo-Menot
