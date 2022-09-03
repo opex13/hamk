cde6ba2d04a02523e09f54bdef087dbaba5d1058
opex13 <97477580+opex13@users.noreply.github.com>
2022-09-03 22:23:07 +0300

    ok

---
+++ ohjelmointi_perusteet/Osio_2/tehtava1.py
@@ -0,0 +1,5 @@
+#ohjelmointi perusteet osio 2 tehtävä 1
+print("Moi! Annatko kaksi lukua?")
+numeroA = int(input("Tässä tulee eka numero"))
+numeroB = int(input("Tässä tulee toka numero"))
+print("No mitä teemme näillä?")
