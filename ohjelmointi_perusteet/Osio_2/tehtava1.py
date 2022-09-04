dcb9548e6deaaf6e177639fafc0986d43a19a0b5
opex13 <97477580+opex13@users.noreply.github.com>
2022-09-04 15:10:31 +0300

    parentheses after .upper

--- ohjelmointi_perusteet/Osio_2/tehtava1.py
+++ ohjelmointi_perusteet/Osio_2/tehtava1.py
@@ -3,3 +3,26 @@ print("Moi! Annatko kaksi lukua?")
 numeroA = int(input("Tässä tulee eka numero"))
 numeroB = int(input("Tässä tulee toka numero"))
 print("No mitä teemme näillä?")
+plusOperator = "A. plus"
+print(plusOperator)
+miinusOperator = "B. miinus"
+print(miinusOperator)
+jakoOperator = "C. jakolasku"
+print(jakoOperator)
+kertoOperator = "D. kertolasku"
+print(kertoOperator)
+operaatio = input("Anna operaatio:")
+if operaatio.upper == "A" or "PLUS" or "+":
+    print("Valitsit", plusOperator)
+    print(numeroA, "+", numeroB, "=", numeroA+numeroB)
+elif operaatio.upper == "B" or "MIINUS" or "-":
+    print("Valitsit", miinusOperator)
+    print(numeroA, "-", numeroB, "=", numeroA-numeroB)
+elif operaatio.upper == "C" or "/" or ":" or "JAKO":
+    print("Valitsit", jakoOperator)
+    print(numeroA, "/", numeroB, "=", numeroA/numeroB)
+elif operaatio.upper == "D" or "*" or "KERTO":
+    print("Valitsit", kertoOperator)
+    print(numeroA, "*", numeroB, "=", numeroA*numeroB)
+else:
+    print("Valittu väärä operaatio, ehkä näppisvirhe?")
