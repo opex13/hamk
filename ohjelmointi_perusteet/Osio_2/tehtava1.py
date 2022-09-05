3596e87aa95f9a1097189584cb5b3ca51897f841
opex13 <97477580+opex13@users.noreply.github.com>
2022-09-05 21:24:08 +0300

    numeerinen tarkistus

--- ohjelmointi_perusteet/Osio_2/tehtava1.py
+++ ohjelmointi_perusteet/Osio_2/tehtava1.py
@@ -1,30 +1,36 @@
 #ohjelmointi perusteet osio 2 tehtävä 1
 print("Moi! Annatko kaksi lukua?")
-numeroA = int(input("Tässä tulee eka numero"))
-numeroB = int(input("Tässä tulee toka numero"))
-print("No mitä teemme näillä?")
-plusOperator = "A. plus"
-print(plusOperator)
-miinusOperator = "B. miinus"
-print(miinusOperator)
-jakoOperator = "C. jakolasku"
-print(jakoOperator)
-kertoOperator = "D. kertolasku"
-print(kertoOperator)
-operaatio = input("Anna operaatio:")
-print(operaatio)
-print(operaatio.upper())
-if operaatio.upper == "A" or operaatio.upper() == "PLUS" or operaatio == "+":
-    print("Valitsit", plusOperator)
-    print(numeroA, "+", numeroB, "=", numeroA+numeroB)
-elif operaatio.upper() == "B" or operaatio.upper() == "MIINUS" or operaatio == "-":
-    print("Valitsit", miinusOperator)
-    print(numeroA, "-", numeroB, "=", numeroA-numeroB)
-elif operaatio.upper() == "C" or operaatio == "/" or operaatio == ":" or operaatio.upper() == "JAKO":
-    print("Valitsit", jakoOperator)
-    print(numeroA, "/", numeroB, "=", numeroA/numeroB)
-elif operaatio.upper() == "D" or operaatio == "*" or operaatio.upper() == "KERTO":
-    print("Valitsit", kertoOperator)
-    print(numeroA, "*", numeroB, "=", numeroA*numeroB)
+numeroA = input("Tässä tulee eka numero")
+numeroB = input("Tässä tulee toka numero")
+#print(numeroA.isnumeric())
+#tarkistetaan että molemmat syötteet ovat numerisia:
+if numeroA.isnumeric() != True and numeroB.isnumeric() != True:
+    print("Osan tehdä laskut vain numeroilla, sorry!")
+    exit()
 else:
-    print("Valittu väärä operaatio, ehkä näppisvirhe?")
+    print("No mitä teemme näillä?")
+    plusOperator = "A. plus"
+    print(plusOperator)
+    miinusOperator = "B. miinus"
+    print(miinusOperator)
+    jakoOperator = "C. jakolasku"
+    print(jakoOperator)
+    kertoOperator = "D. kertolasku"
+    print(kertoOperator)
+    operaatio = input("Anna operaatio:")
+    #print(operaatio) #debug
+    #print(operaatio.upper()) #debug
+    if operaatio.upper == "A" or operaatio.upper() == "PLUS" or operaatio == "+":
+        print("Valitsit", plusOperator)
+        print(numeroA, "+", numeroB, "=", numeroA+numeroB)
+    elif operaatio.upper() == "B" or operaatio.upper() == "MIINUS" or operaatio == "-":
+        print("Valitsit", miinusOperator)
+        print(numeroA, "-", numeroB, "=", numeroA-numeroB)
+    elif operaatio.upper() == "C" or operaatio == "/" or operaatio == ":" or operaatio.upper() == "JAKO":
+        print("Valitsit", jakoOperator)
+        print(numeroA, "/", numeroB, "=", numeroA/numeroB)
+    elif operaatio.upper() == "D" or operaatio == "*" or operaatio.upper() == "KERTO":
+        print("Valitsit", kertoOperator)
+        print(numeroA, "*", numeroB, "=", numeroA*numeroB)
+    else:
+        print("Valittu väärä operaatio, ehkä näppisvirhe?")
