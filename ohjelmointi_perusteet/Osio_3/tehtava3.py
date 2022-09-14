8de335564d585399bf4893d84a84618e8ad27e9c
Vadim Bashlov <opexinbox@gmail.com>
2022-09-14 21:35:13 +0300

    name correction

---
+++ ohjelmointi_perusteet/Osio_3/tehtava3.py
@@ -0,0 +1,24 @@
+#3. Lottoamisen kannattavuus (Pakollinen)
+from random import randrange
+seitsemanOikea = [4,5,9,10,15,31,40] #no... seitsemän oikein lista
+generoituLista = []
+rivit = 0
+def tuplatPois(x):
+    return list(dict.fromkeys(x))
+while seitsemanOikea not in generoituLista:
+#for rivit in range(10000):
+    generoituLista.clear()
+    for x in range(7):
+        lottonumero = randrange(1,41)
+        generoituLista.append(lottonumero)
+        generoituLista.sort()
+        generoituLista = tuplatPois(generoituLista)
+        #print(generoituLista)
+    #print(generoituLista)
+    print(rivit)
+    #print(lottonumero)
+    rivit += 1
+print(generoituLista)
+print(lottonumero)
+print(rivit)
+set(generoituLista)
