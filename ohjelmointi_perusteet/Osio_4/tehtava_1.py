08c43e636937dfbec417cd840551e7ae27951368
Vadim Bashlov <opexinbox@gmail.com>
2022-09-19 22:54:34 +0300

    korjattu tehtävä 1

--- ohjelmointi_perusteet/Osio_4/tehtava_1.py
+++ ohjelmointi_perusteet/Osio_4/tehtava_1.py
@@ -1,14 +1,29 @@
 #1. Numeroiden laskentaa (Pakollinen)
+def tulo(a, b):
+    vastauksienLista.append(a*b)
+
+def osamaara(a, b):
+    vastauksienLista.append(a/b)
+
+def erotus(a, b):
+    vastauksienLista.append(a-b)
+
+def summa(a, b):
+    vastauksienLista.append(a+b)
+    
+def numeroidenLaskenta(a,b):
+    summa(a, b)
+    erotus(a, b)
+    osamaara(a, b)
+    tulo(a, b)
+    return vastauksienLista
+
+def vastauksienTulostus(z):
+    print("numeroiden summa on",z[0],"erotus on",z[1],"osamäärä",z[2],"ja tulo on",z[3])
+    
+#actual code starts here
 numeroA = int(input("Annatko numero A?"))
 numeroB = int(input("Annatko numero B?"))
 vastauksienLista = []
-def numeroidenLaskenta(a,b):
-    vastauksienLista.append(a+b)
-    vastauksienLista.append(a-b)
-    vastauksienLista.append(a/b)
-    vastauksienLista.append(a*b)
-    return vastauksienLista
-def vastauksienTulostus(z):
-    print(z)
 numeroidenLaskenta(numeroA,numeroB)
 vastauksienTulostus(vastauksienLista)
