cb7c2ba69ae4d857e7e95463bd68b2d0f636ed9d
Vadim Bashlov <opexinbox@gmail.com>
2022-09-28 22:03:42 +0300

    Tehtävä tili + korko

--- ohjelmointi_perusteet/Osio_5/tehtava_1.py
+++ ohjelmointi_perusteet/Osio_5/tehtava_1.py
@@ -1,1 +1,10 @@
 #1. Tili (Pakollinen)
+
+from tili import tili
+
+omaTili = tili()
+omaTili.tiliote()
+omaTili.talleta(float(input("paljonko laitetaan tilille?")))
+omaTili.nosta(float(input("paljonko otetaan käteisenä?")))
+omaTili.korkoaKorolle(float(input("paljonko on korkotasoa nyt?")),int(input("kuinka moneksi vuodeksi sijoitat?")))
+#omaTili.tiliote()
--- ohjelmointi_perusteet/Osio_5/tili.py
+++ ohjelmointi_perusteet/Osio_5/tili.py
@@ -1,3 +1,26 @@
 class tili:
     saldo = 0
     
+    def __init__(self):
+        self.saldo = 0
+        
+    def talleta(self, paljonko):
+        self.saldo = self.saldo + paljonko
+        print("tilille talletettu",paljonko,"saldo on", self.saldo)
+    
+    def nosta(self, paljonko):
+        if(paljonko > self.saldo):
+            print("ei ole katetta")
+        else:
+            self.saldo = self.saldo - paljonko  
+            print("nostettu",paljonko,"jäljellä oleva saldo on",self.saldo)
+        
+    def tiliote(self):
+        print(self.saldo)
+        
+    def korkoa(self, korkoTaso):
+        self.saldo = self.saldo*(1+(korkoTaso/100))
+        
+        #kaava [P(1+i)n]-P (Jossa P=pääoma, i=nimellinen vuosittainen korko prosentteina ja n=korkojaksojen määrä)
+    def korkoaKorolle(self, i, n):
+        print("Korkoa korolle olisi",round((self.saldo*(1+i/100)**n)-self.saldo,2),"€")
