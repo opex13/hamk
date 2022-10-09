bc47ea567a427c21b705b27028c2b10dc5206def
Vadim Bashlov <opexinbox@gmail.com>
2022-10-09 12:07:05 +0300

    päätiedosto aloitus

--- ohjelmointi_perusteet/Projekti/lib.py
+++ ohjelmointi_perusteet/Projekti/lib.py
@@ -1,11 +1,16 @@
 # tässä tiedostossa ovat pelin käyttämät luokkaat
+from unicodedata import name
+
+
 class player:
     money = 0
     energy = 0
+    name = ""
     
     def __init__(self):
         self.money = 0
         self.energy = 10
+        name = "player1"
     
     def newRound():
         self.energy = 10
@@ -26,3 +31,7 @@ class player:
         else: 
             self.energy -= x
             print("Sinulla on", x, "energiaa jäljellä")
+
+    def stats():
+        print("energiaa:", self.energy, "rahaa:", self.money)
+    
---
+++ ohjelmointi_perusteet/Projekti/the_game.py
@@ -0,0 +1,8 @@
+# Python projekti vanhan pelin inspioimana - https://jonesinthefastlane.com/
+
+from lib import player
+
+player1 = player()
+
+player1(stats)
+
