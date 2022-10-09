db1120a0cd5725196f6e19d6b920e7f253db805e
Vadim Bashlov <opexinbox@gmail.com>
2022-10-09 23:09:09 +0300

    eka toimiva versio

---
+++ ohjelmointi_perusteet/Projekti/gameClasses.py
@@ -0,0 +1,66 @@
+# tässä tiedostossa ovat pelin käyttämät luokkaat
+from calendar import week
+from random import randint
+from tkinter import W
+
+
+class player:
+    money = 0
+    energy = 0
+    name = ""
+    weeks = 0
+    playerId = 0
+    
+    def __init__(self):
+        self.money = 0
+        self.energy = 10
+        name = "player1"
+    
+    def stats(self): #tulosta pelaajan tiedot
+        print("pelajan",self.name, "energiaa:", self.energy, "rahaa:", self.money, "viikko", self.weeks)
+        
+    def setName(self, givenName): #aseta uuden nimen
+        self.name = givenName
+    
+    def newRound(self): #uusi kierros
+        self.energy = 10
+        self.stats
+        self.weeks += 1
+        
+    def work(self, earnedMoney, spendEnergy): #työn tekeminen
+        if self.energy < spendEnergy:
+            print("sinulla ei ole riittävästi energiaa, täytyy levätä")
+        else:
+            self.money += earnedMoney
+            self.energy -= spendEnergy
+        return(earnedMoney, spendEnergy)
+        self.stats
+        
+    def spendMoney(self, x): #ostaminen
+        if(x > self.money):
+            print("ei ole rahaa ostoksille")
+        else: 
+            self.money -= x
+            print("Teit ostoksen", x, "summalla, tililläsi", self.money, "rahaa")
+            
+    def spendEnergy(self, x): #energian käyttö
+        if(x > self.energy):
+            print("ei ole riitävästi energiaa")
+        else: 
+            self.energy -= x
+            print("Sinulla on", x, "energiaa jäljellä")
+
+
+    def lotto(self, ticketsToBuy): #lottoa
+        if ticketsToBuy > self.money:
+            print("ei ole rahaa pelamiseen")
+        else:
+            self.money -= ticketsToBuy
+            if randint(1,100) > 95:
+                ticketWon = randint(1,100)
+                self.money += ticketWon
+                print("olet voittanut", ticketWon)
+            else:
+                print("tuhlasit", ticketsToBuy,"rahaa")
+            player.stats
+    
--- ohjelmointi_perusteet/Projekti/the_game.py
+++ ohjelmointi_perusteet/Projekti/the_game.py
@@ -1,8 +1,39 @@
 # Python projekti vanhan pelin inspioimana - https://jonesinthefastlane.com/
+from gameClasses import player
+from random import randrange
 
-from lib import player
+#tässä yritetty luoda dynaaminen pelaajien luonti
+#playersInGame = int(input("Tervetuloa peliin! Paljonko pelaajia?"))
+#
+#PlayersDict = {}
+#
+#for x in range(playersInGame):
+#    PlayersDict[x] = str("player",str(x+1))
+#    print(PlayersDict)
 
 player1 = player()
+player1.setName(input("kuka on ensimmäinen pelaja?"))
+player2 = player()
+player2.setName(input("kuka on toinen pelaja?"))
+player1.stats()
+player2.stats()
 
-player1(stats)
+playersList = []
+playersList.append(player1)
+playersList.append(player2)
+#print(playersList)
 
+winGoal = int(input("Paljonko rahaa pitää kerätä voittamiseen?"))
+for playerIsPlaying in playersList:                  #silmukka pelaajin listalle pyörimiseen
+    while winGoal > playerIsPlaying.money:           #tarkistus onko rahat kerätty riitävästi
+        for playerIsPlaying in playersList:          #silmukka pellajin
+            print("Mitä", playerIsPlaying.name, "tänään voisi tehdä?")
+            roundInput = int(input("1. Tehdä töitä. 2. Levätä 3. Pelata lottoa"))
+            if roundInput == 1:
+                playerIsPlaying.work(randrange(1,5),randrange(1,5))
+                playerIsPlaying.stats()
+            elif roundInput == 3:
+                playerIsPlaying.lotto(int(input("paljonko rivia ostetaan?")))
+            else:
+                playerIsPlaying.newRound()
+print(playerIsPlaying.name, "voitti")
